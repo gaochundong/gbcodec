2a08cbe2ef19a41116ee8367fd2acb91b857c11f
Dennis Gao <gaochundong@hotmail.com>
2020-03-15 18:59:05 +0800

    把默认值暴露出来留出扩展空间

--- netty-codec-jt808/src/main/java/ai/sangmado/gbcodec/jt808/codec/JT808MessageCodec.java
+++ netty-codec-jt808/src/main/java/ai/sangmado/gbcodec/jt808/codec/JT808MessageCodec.java
@@ -8,12 +8,15 @@ import io.netty.channel.CombinedChannelDuplexHandler;
  * JT808 协议编解码器
  */
 @SuppressWarnings("FieldCanBeLocal")
-public final class JT808MessageCodec
-        extends CombinedChannelDuplexHandler<JT808MessageDecoder, JT808MessageEncoder<JT808MessagePacket>> {
+public final class JT808MessageCodec extends CombinedChannelDuplexHandler<JT808MessageDecoder, JT808MessageEncoder<JT808MessagePacket>> {
     private ISpecificationContext sctx;
 
     public JT808MessageCodec(ISpecificationContext sctx) {
+        this(sctx, new JT808MessageDecoder(sctx), new JT808MessageEncoder<>(sctx));
+    }
+
+    public JT808MessageCodec(ISpecificationContext sctx, JT808MessageDecoder decoder, JT808MessageEncoder<JT808MessagePacket> encoder) {
         this.sctx = sctx;
-        init(new JT808MessageDecoder(sctx), new JT808MessageEncoder<>(sctx));
+        init(decoder, encoder);
     }
 }
--- netty-codec-jt809/src/main/java/ai/sangmado/gbcodec/jt809/codec/JT809MessageCodec.java
+++ netty-codec-jt809/src/main/java/ai/sangmado/gbcodec/jt809/codec/JT809MessageCodec.java
@@ -8,12 +8,15 @@ import io.netty.channel.CombinedChannelDuplexHandler;
  * JT809 协议编解码器
  */
 @SuppressWarnings("FieldCanBeLocal")
-public final class JT809MessageCodec
-        extends CombinedChannelDuplexHandler<JT809MessageDecoder, JT809MessageEncoder<JT809MessagePacket>> {
+public final class JT809MessageCodec extends CombinedChannelDuplexHandler<JT809MessageDecoder, JT809MessageEncoder<JT809MessagePacket>> {
     private ISpecificationContext sctx;
 
     public JT809MessageCodec(ISpecificationContext sctx) {
+        this(sctx, new JT809MessageDecoder(sctx), new JT809MessageEncoder<>(sctx));
+    }
+
+    public JT809MessageCodec(ISpecificationContext sctx, JT809MessageDecoder decoder, JT809MessageEncoder<JT809MessagePacket> encoder) {
         this.sctx = sctx;
-        init(new JT809MessageDecoder(sctx), new JT809MessageEncoder<>(sctx));
+        init(decoder, encoder);
     }
 }
